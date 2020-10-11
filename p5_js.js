3112ee310cd18c4d46cc351d96b19571641b1fa6
karlaguerreiro <59694318+karlaguerreiro@users.noreply.github.com>
2020-10-11 18:22:52 -0300

    atualizando ping

--- p5_js.js
+++ p5_js.js
@@ -1,122 +1,122 @@
-const TAMANHO_TILE = 50;
+const TAMANHO_TILE = 26;
 var socket = io()
-class ElementoMapa{
+class ElementoMapa {
 
-    constructor(posx,posy,cor,tam){
-        this.x =posx;
+    constructor(posx, posy, cor, tam) {
+        this.x = posx;
         this.y = posy;
         this.cor = cor;
         this.tam = tam;
 
 
-        this.draw = function(){
+        this.draw = function () {
             fill(this.cor);
-            square( this.x * this.tam , this.y * this.tam, this.tam );
+            square(this.x * this.tam, this.y * this.tam, this.tam);
         }
 
     }
 }
 
-class BlocoAzul extends ElementoMapa{
+class BlocoAzul extends ElementoMapa {
 
-    constructor(posx,posy){
-        super(posx,posy,'blue',TAMANHO_TILE);
+    constructor(posx, posy) {
+        super(posx, posy, 'blue', TAMANHO_TILE);
     }
 }
 
-class BlocoVermelho extends ElementoMapa{
+class BlocoVermelho extends ElementoMapa {
 
-    constructor(posx,posy){
-        super(posx,posy,'red',TAMANHO_TILE);
-    }
-}
-
-
-class BlocoAmarelo extends ElementoMapa{
-
-    constructor(posx,posy){
-        super(posx,posy,'yellow',TAMANHO_TILE);
-    }
-}
-
-class BlocoPreto extends ElementoMapa{
-
-    constructor(posx,posy){
-        super(posx,posy,'black',TAMANHO_TILE);
+    constructor(posx, posy) {
+        super(posx, posy, 'red', TAMANHO_TILE);
     }
 }
 
 
-class Player extends BlocoVermelho{
+class BlocoAmarelo extends ElementoMapa {
 
-    constructor(id,posx,posy){
-        super(posx,posy);
+    constructor(posx, posy) {
+        super(posx, posy, 'yellow', TAMANHO_TILE);
+    }
+}
+
+class BlocoPreto extends ElementoMapa {
+
+    constructor(posx, posy) {
+        super(posx, posy, 'black', TAMANHO_TILE);
+    }
+}
+
+
+class Player extends BlocoVermelho {
+
+    constructor(id, posx, posy) {
+        super(posx, posy);
         this.id = id;
     }
 
-    move(tecla){
-        if ( tecla==UP_ARROW    && this.canMove(this.x, this.y-1) ) this.y--;
-        if ( tecla==DOWN_ARROW  && this.canMove(this.x, this.y+1) ) this.y++;
-        if ( tecla==LEFT_ARROW  && this.canMove(this.x-1, this.y) ) this.x--;
-        if ( tecla==RIGHT_ARROW && this.canMove(this.x+1, this.y) ) this.x++;
+    move(tecla) {
+        if (tecla == UP_ARROW && this.canMove(this.x, this.y - 1)) this.y = this.y - 2;
+        //if (tecla == DOWN_ARROW && this.canMove(this.x, this.y + 1)) this.y++;
+        if (tecla == LEFT_ARROW && this.canMove(this.x - 1, this.y)) this.x--;
+        if (tecla == RIGHT_ARROW && this.canMove(this.x + 1, this.y)) this.x++;
     }
 
-    update(){
-    
+    update() {
+        if (this.canMove(this.x, this.y + 1)) this.y++;
     }
 
-    canMove(newX, newY){
+    canMove(newX, newY) {
         return !maps[newY][newX] || maps[newY][newX] == 2;
     }
 }
 
-class Inimigo extends BlocoAmarelo{
+class Inimigo extends BlocoAmarelo {
 
-  constructor(posx,posy){
-      super(posx,posy);
-  }
+    constructor(posx, posy) {
+        super(posx, posy);
+    }
 
-  move(){
-    let mover = parseInt(random(1, 5)) ;
-      //console.log(mover);
+    move() {
+        let mover = parseInt(random(1, 5));
+        //console.log(mover);
 
-      setTimeout(()=>{
-        if ( mover == 1    && this.canMove(this.x, this.y-1) ) this.y--;
-        if ( mover == 2  && this.canMove(this.x, this.y+1) ) this.y++;
-        if ( mover == 3  && this.canMove(this.x-1, this.y) ) this.x--;
-        if ( mover == 4 && this.canMove(this.x+1, this.y) ) this.x++;
-      },random(0, 1000));
-      
-  }
+        setTimeout(() => {
+            if (mover == 1 && this.canMove(this.x, this.y - 1)) this.y--;
+            if (mover == 2 && this.canMove(this.x, this.y + 1)) this.y++;
+            if (mover == 3 && this.canMove(this.x - 1, this.y)) this.x--;
+            if (mover == 4 && this.canMove(this.x + 1, this.y)) this.x++;
+        }, random(0, 1000));
 
-  
+    }
 
-  canMove(newX, newY){
-      return !maps[newY][newX] || maps[newY][newX] == 2;
-  }
+
+
+    canMove(newX, newY) {
+        return !maps[newY][newX] || maps[newY][newX] == 2;
+    }
 }
 
 
 var maps = [
-    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1],
-    [1,0,1,1,1,1,1,0,0,1,1,1,1,1,1,1,0,0,1,1,1,1,1,0,1],
-    [1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1],
-    [1,1,1,1,1,0,1,0,0,1,1,1,2,1,1,1,0,0,1,0,1,1,1,1,1],
-    [1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1],
-    [1,1,1,1,1,0,1,0,0,1,0,0,0,0,0,1,0,0,1,0,1,1,1,1,1],
-    [1,0,0,0,0,0,1,0,0,1,1,1,1,1,1,1,0,0,1,0,0,0,0,0,1],
-    [1,0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,1],
-    [1,0,1,0,0,0,0,0,0,1,1,1,1,1,1,1,0,0,0,0,0,0,1,0,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ];
 
 var elementos = [];
 
-var players = [] ;
+var players = [];
 var jogador;
 //var inimigo = new Inimigo(10,7)
 
@@ -125,40 +125,47 @@ var jogador;
 
 
 
-function setup(){
+function setup() {
     const blocos = [BlocoPreto, BlocoAzul, BlocoAmarelo, BlocoVermelho];
-    jogador = new Player(parseInt(random(10)),1,7);
-    createCanvas(1280, 720);
+    jogador = new Player(parseInt(random(10)), 1, 7);
+    createCanvas(650, 370);
 
     for (let i = 0; i < maps.length; i++) {
-        for(let j=0;j< maps[i].length;j++) {
-            elementos.push(new blocos[maps[i][j]](j,i));
+        for (let j = 0; j < maps[i].length; j++) {
+            elementos.push(new blocos[maps[i][j]](j, i));
         }
     }
 }
 
-function draw(){
-    
+function draw() {
+
+    if (keyIsPressed === true) {
+        jogador.move(keyCode);
+    }
+    jogador.update();
+
+
     for (let i = 0; i < elementos.length; i++) {
         elementos[i].draw();
     }
 
     jogador.draw();
-   // inimigo.draw();
-   // inimigo.move();
-   // console.log("Total players",players.length);
-    if(players.length){
-        for(let i=0;i<players.length;i++){
+
+    // inimigo.draw();
+    // inimigo.move();
+    // console.log("Total players",players.length);
+    if (players.length) {
+        for (let i = 0; i < players.length; i++) {
             //console.log(players[i],players[i].x);
             fill(players[i].cor);
-            square( players[i].x * players[i].tam , players[i].y * players[i].tam, players[i].tam );
+            square(players[i].x * players[i].tam, players[i].y * players[i].tam, players[i].tam);
         }
     }
-    
+
 }
 
-function keyPressed(){
-    jogador.move(keyCode);
-    
+function keyPressed() {
+
+    console.log("Precionando ", keyCode);
 }
 
